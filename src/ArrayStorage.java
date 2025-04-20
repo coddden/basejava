fe4449050ef36532bf88dcf1058716da137365d8
Denis <coddden@gmail.com>
2025-04-20 23:45:27 +0500

    HW_1 v1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,53 @@
+import java.util.Arrays;
+
 /**
- * Array based storage for Resumes
+ * Array based storage for Resumes.
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
 
-    void clear() {
-    }
-
-    void save(Resume r) {
-    }
-
-    Resume get(String uuid) {
-        return null;
-    }
-
-    void delete(String uuid) {
-    }
+    private static final int STORAGE_SIZE = 10000;
+    private final Resume[] storage = new Resume[STORAGE_SIZE];
+    private int resumeCount = 0;
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, resumeCount);
     }
 
-    int size() {
-        return 0;
+    public Resume get(String uuid) {
+        for (Resume resume : getAll()) {
+            if (uuid.equals(resume.toString())) {
+                return resume;
+            }
+        }
+        return null;
+    }
+
+    public void save(Resume r) {
+        storage[resumeCount++] = r;
+    }
+
+    public void delete(String uuid) {
+        for (int i = 0; i < resumeCount; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                resumeCount--;
+                if (i != resumeCount) {
+                    System.arraycopy(storage, i + 1, storage, i, resumeCount - i);
+                }
+                storage[resumeCount] = null;
+                break;
+            }
+        }
+    }
+
+    public void clear() {
+        Arrays.fill(storage, 0, resumeCount, null);
+        resumeCount = 0;
+    }
+
+    public int size() {
+        return STORAGE_SIZE - resumeCount;
     }
 }
