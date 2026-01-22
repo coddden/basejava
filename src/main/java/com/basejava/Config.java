c333d9876af30d62a483ef6401159ba1d890ed41
Den <denconcept.two@yandex.ru>
2026-01-22 17:05:49 +0500

    hw_15: Update Config to read properties

--- src/main/java/com/basejava/Config.java
+++ src/main/java/com/basejava/Config.java
@@ -3,26 +3,30 @@ package com.basejava;
 import com.basejava.storage.SqlStorage;
 import com.basejava.storage.Storage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File("config/resumes.properties");
+    
     private static final Config INSTANCE = new Config();
     private final File storageDir;
     private final Storage storage;
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = Config.class.getClassLoader().getResourceAsStream("resumes.properties")) {
+            if (is == null) {
+                throw new IllegalStateException("resumes.properties not found in classpath");
+            }
             Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
-            storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"),
+            storage = new SqlStorage(
+                    props.getProperty("db.url"),
+                    props.getProperty("db.user"),
                     props.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file ");
         }
     }
 
